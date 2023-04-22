2d99e7f5980825fde1ee5b15be0f89fb683683e9
Ivan Ankudinov <ivan.ankudinov97@gmail.com>
2023-04-22 23:19:41 +0300

    bot.1.4

--- quetions.py
+++ quetions.py
@@ -10,7 +10,7 @@ from list_words import get_list_words
 from word_class import Word
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
 )
 logger = logging.getLogger(__name__)
 
@@ -19,9 +19,36 @@ WORDS = get_list_words()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Inform user about what this bot can do"""
+    await update.message.reply_text(help_command())
+
+
+def help_command():
+    """Отправляет сообщение когда получена команда /help"""
+    return '''/admin - поможет тебе узнать имена создателей этого проекта.
+/work_time - укажет время работы нашего бота.
+/address - поможет узнать адрес нашего офиса.
+/phone - поможет позвонить админам и пожаловаться на проект :)
+/work - команда, при введении которой открывается возможность поставить ударение в слове"'''
+
+
+async def admin(update, contest):
+    await update.message.reply_text(f'Это проект Анкудинова Степана и Дудича Михаила')
+
+
+# Напишем соответствующие функции.
+
+async def address(update, context):
     await update.message.reply_text(
-        "Введите команду /quiz для того, чтобы поставить ударение в случайном слове"
-    )
+        "У нас пока нет офиса, но если вы дадите нам денег, он обязательно появится :)")
+
+
+async def phone(update, context):
+    await update.message.reply_text("Телефон: +7(908)736-44-78\nТелефон: +7(986)758-45-60")
+
+
+async def work_time(update, context):
+    await update.message.reply_text(
+        "Время работы: Мы не работаем, пока нам не платят")
 
 
 async def quiz(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -55,20 +82,17 @@ async def receive_quiz_answer(update: Update, context: ContextTypes.DEFAULT_TYPE
         await context.bot.stop_poll(quiz_data["chat_id"], quiz_data["message_id"])
 
 
-async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Display a help message"""
-    await update.message.reply_text("/quiz - команда, при введении которой открывается " \
-                                    "возможность поставить ударение в слове")
-
-
 def main() -> None:
     """Run bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(
         "6260730894:AAGwAXctln7jY9cQPEx-X6RZSqOQDqFKX_Y").build()
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("quiz", quiz))
-    application.add_handler(CommandHandler("help", help_handler))
+    application.add_handler(CommandHandler("work", quiz))
+    application.add_handler(CommandHandler("admin", admin))
+    application.add_handler(CommandHandler("address", address))
+    application.add_handler(CommandHandler("phone", phone))
+    application.add_handler(CommandHandler("work_time", work_time))
     application.add_handler(PollHandler(receive_quiz_answer))
 
     # Run the bot until the user presses Ctrl-C
